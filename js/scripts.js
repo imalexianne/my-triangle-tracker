1b17703f0bc7f95f3fa21fdd1dba6f9766e3469c
Imanirakarama Alexianne <imanirax@yahoo.fr>
2018-12-13 18:11:18 +0200

    change Number to value1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// function myFunction() {
-    // var firstString=document.getElementById("myNumber").value;
-    // alert(firstString);
-// }
+//  function myFunction() {
+    //  var firstString=document.getElementById("myNumber").value;
+    //  alert(firstString);
+//  }
