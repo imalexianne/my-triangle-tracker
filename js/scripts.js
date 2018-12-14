0ab022ab7e6d7f1d23077a5f95d83bc46f9013cc
Imanirakarama Alexianne <imanirax@yahoo.fr>
2018-12-14 14:59:16 +0200

    add console.log to the javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,22 @@
             value[0]===value[2] && 
             value[1]===value[2]){
             alert("equilateral triangle");
+            console.log("equilateral triangle");
         }
         else if (value[0]===value[1]||
             value[0]===value[2]||
             value[1]===value[2]){
             alert("isoceles triangle");
+            console.log("isoceles triangle");
         }
         else {
-            alert("scalene triangle")
+            alert("scalene triangle");
+            console.log("scalene triangle");
         }
     }
        
     else {
         alert("Not a triangle");
+        console.log("Not a triangle");
     } 
 }
