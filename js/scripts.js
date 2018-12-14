970546dfc4387d368322bcebb702a40aa3e4f8e7
Imanirakarama Alexianne <imanirax@yahoo.fr>
2018-12-14 10:23:35 +0200

    add function to javascripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
  function myFunction() {
-    //  var firstString=document.getElementById("myNumber").value;
-    //  alert(firstString);
+//  var firstValue=document.getElementById("myNumber").value1;
+//  var secondValue=document.getElementById("myNumber").value2;
+//  var thirdValue=document.getElementById("myNumber").value3;
+    //  alert(firstValue,secondValue,thirdValue);
 //  }
+//  if (value1==value2 && value1==value3 && value2==value3){
+    // alert("equilateral triangle");
+    // }
+    // else if (value1==value2||value1==value3||value2==value3){
+    // alert("isoceles triangle");
+    // }
+    // else if (value1+value2 > value3 && value1+ value3>value2 && value2+value3>value1) {
+    // alert("Scalene triangle");
+// }
+// else {
+// alert("Not a triangle");
+// } 
