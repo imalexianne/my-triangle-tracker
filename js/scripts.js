bfd28eadf2a763ecc07d696cc93ee372c393653c
Imanirakarama Alexianne <imanirax@yahoo.fr>
2018-12-14 13:29:51 +0200

    add  if  (value1+value2 > value3 && to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
  function myFunction() {
     var value1=parseInt(document.getElementById("value1").value);
     var value2=parseInt(document.getElementById("value2").value); 
-    /*var value3=parseInt(document.getElementById("value3").value);
+    var value3=parseInt(document.getElementById("value3").value);
     
     if  (value1+value2 > value3 &&
          value1+ value3>value2 && 
          value2+value3>value1){
-        if (value1===value2 && 
+        /*if (value1===value2 && 
             value1===value3 && 
             value2===value3){
             alert("equilateral triangle");
