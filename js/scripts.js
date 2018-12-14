648329081ff708f19d1f623c0a4d09a313c6f2a2
Imanirakarama Alexianne <imanirax@yahoo.fr>
2018-12-14 13:21:38 +0200

    var value1=parseInt(document.getElementById(value1).value);

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
  function myFunction() {
-//  var firstValue=document.getElementById("myNumber").value1;
-//  var secondValue=document.getElementById("myNumber").value2;  var thirdValue=document.getElementById("myNumber").value3;
-//   alert(firstValue,secondValue,thirdValue);
-//  }
-//  if (value1==value2 && value1==value3 && value2==value3){
-    //  alert("equilateral triangle");
-    //  }
-    // else if (value1==value2||value1==value3||value2==value3){
-    //  alert("isoceles triangle");
-    // }
-    // else if (value1+value2 > value3 && value1+ value3>value2 && value2+value3>value1) {
-    // alert("Scalene triangle");
-// }
-// else {
-// alert("Not a triangle");
-// } 
+    var value1=parseInt(document.getElementById("value1").value);
+    /*var value2=parseInt(document.getElementById("value2").value); 
+    var value3=parseInt(document.getElementById("value3").value);
+    
+    if  (value1+value2 > value3 &&
+         value1+ value3>value2 && 
+         value2+value3>value1){
+        if (value1===value2 && 
+            value1===value3 && 
+            value2===value3){
+            alert("equilateral triangle");
+        }
+        else if (value1===value2||
+            value1===value3||
+            value2===value3){
+            alert("isoceles triangle");
+        }
+        else {
+            alert("scalene triangle")
+        }
+    }
+       
+    else {
+        alert("Not a triangle");
+    } 
+}*/
