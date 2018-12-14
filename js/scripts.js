09ed469fd85d0d9d42544835aa8f269f834bdf67
Imanirakarama Alexianne <imanirax@yahoo.fr>
2018-12-14 13:26:45 +0200

    add  var value2=parseInt(document.getElementById(value2).value); to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
  function myFunction() {
     var value1=parseInt(document.getElementById("value1").value);
-    /*var value2=parseInt(document.getElementById("value2").value); 
-    var value3=parseInt(document.getElementById("value3").value);
+    var value2=parseInt(document.getElementById("value2").value); 
+    /*var value3=parseInt(document.getElementById("value3").value);
     
     if  (value1+value2 > value3 &&
          value1+ value3>value2 && 
